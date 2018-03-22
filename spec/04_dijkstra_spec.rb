799e2575bd13d09e809aec42ecee8bcd807791ab
Ned Ruggeri <ruggeri@self-loop.com>
2018-03-21 21:15:55 -0700

    keep writing specs for insertion

--- spec/04_dijkstra_spec.rb
+++ spec/04_dijkstra_spec.rb
@@ -60,7 +60,8 @@ end
 describe "#add_vertex_edge" do
   let(:atlanta) { $vertices["ATL"] }
   let(:denver) { $vertices["DEN"] }
-  let(:lax) { $vertices["LAX"] }
+  let(:los_angeles) { $vertices["LAX"] }
+  let(:dallas) { $vertices["DFW"] }
 
   let(:atl_den) { $edges["ATL_DEN"] }
   let(:atl_lax) { $edges["ATL_LAX"] }
@@ -81,7 +82,7 @@ describe "#add_vertex_edge" do
     Fiber.new { add_vertex_edges(result_map, fringe, extracted_entry) }
   end
 
-  describe "adding first edge (atl to den)" do
+  describe "inserting first edge (atl to den)" do
     let(:expected_new_entry) do
       ResultEntry.new(
         denver,
@@ -94,7 +95,7 @@ describe "#add_vertex_edge" do
       Fringe.new({ denver => expected_new_entry })
     end
 
-    it "doesn't change result" do
+    it "doesn't change result map" do
       msg = fiber.resume
 
       expect(msg.name).to eq(:edge_consideration)
@@ -114,6 +115,141 @@ describe "#add_vertex_edge" do
     it "tells us this was an insert" do
       msg = fiber.resume
       expect(msg.action).to eq(:insert)
+    end
+  end
+
+  describe "inserting second edge (atl to lax)" do
+    let(:expected_new_entry) do
+      ResultEntry.new(
+        los_angeles,
+        atl_lax,
+        atl_lax.cost
+      )
+    end
+
+    let(:expected_fringe) do
+      Fringe.new(
+        { denver => ResultEntry.new(
+            denver,
+            atl_den,
+            atl_den.cost
+          ),
+          los_angeles => expected_new_entry
+        }
+      )
+    end
+
+    before do
+      # Burn first message (checked it before).
+      _ = fiber.resume
+    end
+
+    it "doesn't change result map" do
+      msg = fiber.resume
+
+      expect(msg.name).to eq(:edge_consideration)
+      expect(msg.result_map.to_hash).to eq(result_map.to_hash)
+    end
+
+    it "builds a new entry" do
+      msg = fiber.resume
+      expect(msg.new_entry.to_hash).to eq(expected_new_entry.to_hash)
+    end
+
+    it "inserts new entry in fringe" do
+      msg = fiber.resume
+      expect(msg.fringe.to_hash).to eq(expected_fringe.to_hash)
+    end
+
+    it "tells us this was an insert" do
+      msg = fiber.resume
+      expect(msg.action).to eq(:insert)
+    end
+  end
+
+  describe "inserting third edge (dfw to atl)" do
+    let(:expected_new_entry) do
+      ResultEntry.new(
+        dallas,
+        dfw_atl,
+        dfw_atl.cost
+      )
+    end
+
+    let(:expected_fringe) do
+      Fringe.new(
+        { denver => ResultEntry.new(
+            denver,
+            atl_den,
+            atl_den.cost
+          ),
+          los_angeles => ResultEntry.new(
+            los_angeles,
+            atl_lax,
+            atl_lax.cost
+          ),
+          dallas => expected_new_entry
+        }
+      )
+    end
+
+    before do
+      # Burn first two messages (checked it before).
+      _ = fiber.resume
+      _ = fiber.resume
+    end
+
+    it "doesn't change result map" do
+      msg = fiber.resume
+
+      expect(msg.name).to eq(:edge_consideration)
+      expect(msg.result_map.to_hash).to eq(result_map.to_hash)
+    end
+
+    it "builds a new entry" do
+      msg = fiber.resume
+      expect(msg.new_entry.to_hash).to eq(expected_new_entry.to_hash)
+    end
+
+    it "inserts new entry in fringe" do
+      msg = fiber.resume
+      expect(msg.fringe.to_hash).to eq(expected_fringe.to_hash)
+    end
+
+    it "tells us this was an insert" do
+      msg = fiber.resume
+      expect(msg.action).to eq(:insert)
+    end
+  end
+
+  describe "completion" do
+    it "finishes after three inserts" do
+      _ = fiber.resume
+      _ = fiber.resume
+      _ = fiber.resume
+
+      new_fringe = fiber.resume
+
+      expected_new_fringe = Fringe.new(
+        { denver => ResultEntry.new(
+            denver,
+            atl_den,
+            atl_den.cost
+          ),
+          los_angeles => ResultEntry.new(
+            los_angeles,
+            atl_lax,
+            atl_lax.cost
+          ),
+          dallas => ResultEntry.new(
+            dallas,
+            dfw_atl,
+            dfw_atl.cost
+          ),
+        }
+      )
+
+      expect(new_fringe.to_hash).to eq(expected_new_fringe.to_hash)
     end
   end
 end
